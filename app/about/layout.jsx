5eaa1c1754a1d5d778edf18086d821e801e0a02e
2joopiter <veryberrylike3@gmail.com>
2024-01-09 16:11:23 +0900

    서브페이지 전용 메타데이터

--- app/about/layout.jsx
+++ app/about/layout.jsx
@@ -1,5 +1,12 @@
 import AboutMenu from '@/components/aboutMenu/aboutMenu';
 
+export const generateMetadata = () => {
+	return {
+		title: 'About Page',
+		description: 'Information of ABC Company including President, crew info',
+	};
+};
+
 export default function Layout({ children }) {
 	return (
 		<div className='layout'>
