c75db6a4f955a3dbef7501ad25bc6be2575a8def
2joopiter <veryberrylike3@gmail.com>
2024-01-09 13:19:43 +0900

    메뉴설정

---
+++ app/gallery/page.jsx
@@ -0,0 +1,9 @@
+import styles from './gallery.module.scss';
+
+export default function Gallery() {
+	return (
+		<main className={styles.gallery}>
+			<h1>Gallery page</h1>
+		</main>
+	);
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Header from './components/header/header';
 import './styles/globals.scss';
 
 export const metadata = {
@@ -8,7 +9,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body>{children}</body>
+			<body>
+				<Header />
+				{children}
+			</body>
 		</html>
 	);
 }
+
+/*
+	Next 에서의 기능별 컴포넌트 구분
+	-page Component: 페이지로 출력될 틀 컴포넌트(about/page.jsx, gallery/page.jsx 같은 것들)
+		+ 따로 설정을 하지 않으면 기본적으로 Server에서 동작되서 prepage로 사전 렌더링
+
+	-Partial Component: 페이지 안쪽에서 호출되면서 부분적인 UI나 기능을 담당하는 컴포넌트 (Compnents>header.jsx, components>popup.jsx 같은 것들)
+*/
