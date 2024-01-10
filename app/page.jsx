911147372e8b2b974a3cc8987b1cd0aa3d6c13ae
2joopiter <veryberrylike3@gmail.com>
2024-01-10 13:16:14 +0900

    placeholder로 초기 이미지 최적화

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ export default function Home() {
 					quality={100}
 					// 속성이 fill일 때 next가 크기 산정하기 어려우므로, 특정 크기 미만의 브라우저폭에 대한 사이즈 크기를 지정하기를 권장
 					sizes='(min-width: 808px) 50vw, 100vw'
-					priority
+					placeholder='blur'
 				/>
 			</div>
 
@@ -40,4 +40,7 @@ export default function Home() {
 	6. Image 컴포넌트 호출시 가급적 width, height 속성을 지정해야 next가 빌드시에 해당 사이즈에 맞는 최적화 작업을 함
 	7. 이미지의 크기가 고정값이 아닐때는 fill 적용하기 (연결되어있는 style의 넓이, 높이를 판단해서 알아서 최적화함)
 	8. fill속성 적용시 추가적으로 하면 좋은 속성 > sizes
+	9. placeholder="blur"로 지정시 (로컬이미지일때) 미리 blur이미지를 먼저 출력하고 전체 이미지 렌더링이 완료되면 변경처리
+	9-1. 주의점: Placeholder 지정시 lazy-loading기능 제거됨, priority 적용시에는 placeholder가 동작안됨
+	10. 처음 마운트 화면에 보일 큰 이미지에 적용할 수 있는 2가지 옵션 (1. Priority / 2. placeholder)
 */
