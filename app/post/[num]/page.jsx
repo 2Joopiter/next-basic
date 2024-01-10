ccd06dcbd4ba90b9423736f8152d3181da4c5be1
2joopiter <veryberrylike3@gmail.com>
2024-01-10 15:35:41 +0900

    Link를 통해 전달되는 params값은 useParams 없이도 기본 props로 전달가능

--- app/post/[num]/page.jsx
+++ app/post/[num]/page.jsx
@@ -1,9 +1,11 @@
 'use client';
-import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 export default function PostDetail({ params }) {
-	const { num } = useParams();
+	// 해당 다이나믹 라우팅 페이지 컴포넌트가 호출된 조건이 Link 컴포넌트에 의해서 호출된 경우라면 useParams 훅을 쓰지 않더라도 자동으로 props를 통해 params 객체가 받아짐
+	// useParams를 쓰는 경우는 Link가 특정 핸들러함수를 통해서 동적으로 호출될때 직접 params 값을 활용할 때
+
+	const { num } = params();
 	const [Post, setPost] = useState(null);
 
 	useEffect(() => {
@@ -18,8 +20,8 @@ export default function PostDetail({ params }) {
 
 	return (
 		<div>
-			<h2>{Post.title}</h2>
-			<p>{Post.body}</p>
+			<h2>{Post?.title}</h2>
+			<p>{Post?.body}</p>
 		</div>
 	);
 }
