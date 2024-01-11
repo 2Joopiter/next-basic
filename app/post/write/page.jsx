547e2e7f7e7d9669b36cb4f13d4ad904ad827977
2joopiter <veryberrylike3@gmail.com>
2024-01-11 16:07:52 +0900

    클라이언트로부터 서버쪽에 글저장 요청 보내기

--- app/post/write/page.jsx
+++ app/post/write/page.jsx
@@ -5,20 +5,37 @@ import clsx from 'clsx';
 
 export default function Write() {
 	const [Post, setPost] = useState({ title: '', content: '' });
-	console.log(Post);
 
 	const handleChange = (e) => {
 		const { name, value } = e.target; //{'title','제목값'}
 		setPost({ ...Post, [name]: value });
 	};
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const response = await fetch('/api/requestPost', {
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(Post),
+		});
+
+		if (response.ok) {
+			alert('글 저장에 성공했습니다.');
+		} else {
+			alert('글 저장에 실패했습니다.');
+		}
+	};
+
 	return (
 		<section className={clsx(styles.write)}>
 			<h1>Write Post</h1>
 
-			<form>
+			<form onSubmit={handleSubmit}>
 				<input type='text' placeholder='title' name='title' value={Post.title} onChange={handleChange} />
-				<textarea name='content' cols='30' rows='3' value={Post.content} onChange={handleChange}></textarea>
+				<textarea name='content' cols='30' rows='3' placeholder='content' value={Post.content} onChange={handleChange}></textarea>
+
+				<button>Send Post</button>
 			</form>
 		</section>
 	);
